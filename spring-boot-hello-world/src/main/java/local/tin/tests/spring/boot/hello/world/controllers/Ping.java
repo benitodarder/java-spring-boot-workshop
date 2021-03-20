f6c2c6de2aa1a09589d7d2c4ca44ef4112a365d9
benitodarder@gmail.com <benitodarder@gmail.com>
2021-03-20 19:55:23 +0100

    Ping with get argumentless, get with arguments, post, post with url form encoded arguments

--- spring-boot-hello-world/src/main/java/local/tin/tests/spring/boot/hello/world/controllers/Ping.java
+++ spring-boot-hello-world/src/main/java/local/tin/tests/spring/boot/hello/world/controllers/Ping.java
@@ -1,14 +1,15 @@
 package local.tin.tests.spring.boot.hello.world.controllers;
 
 import java.util.Date;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import java.util.Map.Entry;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
  
 /**
  *
@@ -18,14 +19,23 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RequestMapping(path = "/ping")
 public class Ping {
 
-    @GetMapping(path = "/get", produces = "text/plain")
+    @GetMapping(path = "/", produces = "text/plain")
     public String helloWorld() {
         return "Hello being, it's " + new Date();
     }
 
-    @PostMapping(path = "/postWithBody", consumes = "text/Plain", produces = "text/Plain")
+    @PostMapping(path = "/postWithBody", consumes = {MediaType.TEXT_PLAIN_VALUE}, produces = "text/Plain")
     public String helloWorldWithBody(@RequestBody String string) {
         return "Hello " + string + ", it's " + new Date();
     }
 
+    @GetMapping(path = "/getWithArguments", produces = "text/plain")
+    public String helloWorldWithParameter(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello " + name + ", it's " + new Date();
+    }
+    
+    @PostMapping(path = "/postURLEncoded", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}, produces = "text/Plain")
+    public String helloWorldUrlEncoded(String name) {
+        return "Hello " + name + ", it's " + new Date();
+    }    
 }
